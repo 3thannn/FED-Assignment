6c0f3978cb962fbd55f3a1a967cf99c8d6b99d8e
jiameelorh <41763337+jiameelorh@users.noreply.github.com>
2020-01-24 20:46:25 +0800

    sdds

--- js/script.js
+++ js/script.js
@@ -7,3 +7,17 @@ $("#swimming-btn").click(function(){
   $("#swimming-pic").slideToggle().delay("3000");
   $("#swimming-info").fadeToggle();
 });
+
+//iframe
+$("#running-web").click(function(){
+  $("#min-web").attr('src', 'ethan.html');
+});
+$("#golf-web").click(function(){
+  $("#min-web").attr('src', 'Dickson.html');
+});
+$("#swimming-web").click(function(){
+  $("#min-web").attr('src', 'jiacheng.html');
+});
+$("#basketball-web").click(function(){
+  $("#min-web").attr('src', 'elvanindex.html');
+});
