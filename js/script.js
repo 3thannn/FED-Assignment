beff4796a7acf6bc3f465b2c031e1d93d96294b0
jiameelorh <41763337+jiameelorh@users.noreply.github.com>
2020-01-24 20:58:05 +0800

    sd

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
 });
 
 //Listening function
+
+//Upcoming Events
 $("#swimming-btn").click(function(){
   $("#swimming-pic").slideToggle().delay("3000");
   $("#swimming-info").fadeToggle();
@@ -11,13 +13,17 @@ $("#swimming-btn").click(function(){
 //iframe
 $("#running-web").click(function(){
   $("#min-web").attr('src', 'ethan.html');
+  $("#info-footer").attr('href', 'ethan.html');
 });
 $("#golf-web").click(function(){
   $("#min-web").attr('src', 'Dickson.html');
+  $("#info-footer").attr('href', 'Dickson.html');
 });
 $("#swimming-web").click(function(){
   $("#min-web").attr('src', 'jiacheng.html');
+  $("#info-footer").attr('href', 'jiacheng.html');
 });
 $("#basketball-web").click(function(){
   $("#min-web").attr('src', 'elvanindex.html');
+  $("#info-footer").attr('href', 'elvanindex.html');
 });
