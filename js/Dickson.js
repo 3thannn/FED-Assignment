7d42acd3c7ce3d22f0844965be67bfb3b321358c
jiameelorh <41763337+jiameelorh@users.noreply.github.com>
2020-01-28 10:06:04 +0800

    sdsdsd

--- js/Dickson.js
+++ js/Dickson.js
@@ -1,7 +1,12 @@
+//Var
+//Golfer Card var
 var abdul = false;
 var mardan = false;
 var quincy = false;
 var displaysec4 = false;
+
+//Quiz var
+var QuestionArray = [];
 
 //Preload function
 $("#section4-1").hide();
@@ -100,41 +105,76 @@ function quizValidation(){
   var counter = 0;
   var name = document.getElementById("user-name").value;
 
-  //Question 1
-  var question1 = [];
-  $(':checkbox:checked').each(function(i){
-    question1[i] = $(this).val();
-    if(question1[i]=="clubs" || question1[i]=="iron" || question1[i]=="wedges"){
-      mark += 1;
+  //Vaidating Name
+  if($.isNumeric(name) == false){
+    //Question 1
+    var question1 = [];
+    $(':checkbox:checked').each(function(i){
+      question1[i] = $(this).val();
+      if(question1[i]=="clubs" || question1[i]=="iron" || question1[i]=="wedges"){
+        mark += 1;
+      }
+      else if(question1[i]=="driver"){
+        mark = 0;
+      }
+      if(mark == 3){
+        QuestionArray.push("q1");
+      }
+    });//Dickson Kuan
+
+    //Question2
+    var radioValue = $("input[name='max-amount-clubs']:checked").val();
+    if(radioValue == "14"){
+        mark += 1;
+        QuestionArray.push("q2");
     }
-    else if(question1[i]=="driver"){
-      mark = 0;
-    }
-  });//Dickson Kuan
 
-  //Question2
-  var radioValue = $("input[name='max-amount-clubs']:checked").val();
-  if(radioValue == "14"){
+    //Question3
+    var question3 =  $("#expensive-clubs").val();
+    if(question3 == "181000"){
       mark += 1;
-  }
+      QuestionArray.push("q3");
+    }
 
-  //Question3
-  var question3 =  $("#expensive-clubs").val();
-  if(question3 == "181000"){
-    mark += 1;
-  }
+    //Question 4
+    var question4 = $("#question4").val()
+    if(question4 == "Callaway"){
+      mark += 1;
+      QuestionArray.push("q4");
+    }
+    //Indicate if each question is correct or incorrect
+    for(i=0;i<QuestionArray.length;i++){
+      switch (QuestionArray[i]){
+        case "q1":
+          document.getElementById('q1-c').innerHTML='Correct';
+          $("#q1-c").css('color','green');
+          break;
+        case "q2":
+          document.getElementById('q2-c').innerHTML='Correct';
+          $("#q2-c").css('color','green');
+          break;
+        case "q3":
+          document.getElementById('q3-c').innerHTML='Correct';
+          $("#q3-c").css('color','green');
+          break;
+        case "q4":
+          document.getElementById('q4-c').innerHTML='Correct';
+          $("#q4-c").css('color','green');
+          break;
+      }
+    }
 
-  //Question 4
-  var question4 = $("#question4").val()
-  if(question4 == "Callaway"){
-    mark += 1;
+    //Animation
+    $("#section5").hide();
+    $("#section5-1").fadeIn("slow");
+    document.getElementById("name").innerHTML = name;
+    document.getElementById("marks").innerHTML = mark;
+    window.location.hash = "#section5-1";
   }
-
-  $("#section5").hide();
-  $("#section5-1").fadeIn("slow");
-  document.getElementById("name").innerHTML = name;
-  document.getElementById("marks").innerHTML = mark;
-  window.location.hash = "#section5-1";
+  else{
+    alert("Please insert a valid name");
+    return false;
+  }
 }
 //End of quiz
 
