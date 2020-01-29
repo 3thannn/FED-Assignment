6374b1052abf56f4271539bf01378705c50dd6ba
jiameelorh <41763337+jiameelorh@users.noreply.github.com>
2020-01-29 09:11:58 +0800

    orjef

--- js/credits.js
+++ js/credits.js
@@ -1,8 +1,36 @@
+//Var
+var loaded = false;
+
 //Preload function
 $("#section1").hide();
+$(".info").hide();
 
 //Listening Fucntion
 $("#azzi").click(function(){
-  $("#section1").slideToggle(1000);
-  $("#name").html('Azzi')
+  LoadSector();
+  $("#name").html('Azzi');
+  $("#elvan-accordion, #dickson-accordion").hide(function(){
+    $("#azzi-accordion").fadeIn(1000);
+  });
 });
+$("#elvan").click(function(){
+  LoadSector();
+  $("#name").html('Elvan');
+  $("#azzi-accordion, #dickson-accordion").hide(function(){
+    $("#elvan-accordion").fadeIn(1000);
+  });
+});
+$("#dickson").click(function(){
+  LoadSector();
+  $("#name").html('Dickson');
+  $("#azzi-accordion, #dickson-accordion").hide(function(){
+    $("#elvan-accordion").fadeIn(1000);
+  });
+});
+//Function
+function LoadSector(){
+  if(loaded == false){
+    $("#section1").slideToggle(500);
+    loaded = true;
+  }
+}
